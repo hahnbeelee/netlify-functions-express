9a4c7de9f78165a294b12c6f621de24eaaf8d638
Hahnbee Lee <hl985@cornell.edu>
2021-09-17 18:13:00 -0400

    new line

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        
         <GitHubCorner url={'https://github.com/netlify-labs/netlify-functions-express'} />
         <header className="App-header">
 
